c4ea1950c897b2ca977d2964a552a0ac384fb32e
Rafael Tivane <luislangabusiness@gmail.com>
2022-10-23 12:44:08 +0200

    create hero panel and add valid avatar url

--- src/components/header/components/avatar/index.jsx
+++ src/components/header/components/avatar/index.jsx
@@ -24,7 +24,7 @@ const Container = () => {
                 <Avatar 
                     alt=""
                     className={classNames(classes.avatar)}
-                    src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F002%2F002%2F403%2Foriginal%2Fman-with-beard-avatar-character-isolated-icon-free-vector.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fhuman-avatar&tbnid=MV9dWdy0ZY_UPM&vet=12ahUKEwib9oyH2_X6AhVJZRoKHdanC6sQMygcegUIARCPAg..i&docid=MSzOgmpmesXuDM&w=7973&h=7974&q=avatar%20icon%20&ved=2ahUKEwib9oyH2_X6AhVJZRoKHdanC6sQMygcegUIARCPAg"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
                 />
             </IconButton>
             <Popover
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,38 @@
 import { Button, Typography } from "@mui/material"
 import classNames from "classnames"
 
+import classes from "./styles.module.css";
 
 const Home = () => {
 
     return (
         <main>
-            <h1 className="font-bold text-4xl">Hello</h1>
+            <section>
+                <div className="bg-blue-500 px-5 pt-4 pb-12">
+                    <Typography
+                        component="h1"
+                        className="font-bold text-2xl text-white">
+                        Dashboard
+                    </Typography>
+                </div>
+                <div className="relative">
+                    <div className={classNames(classes.heroPanel, `absolute bg-white left-5 px-3 py-4`)}>
+                        <Typography
+                            component="h2"
+                            className="">
+                            Seja bem-vindo ao painel de gerenciamento
+                        </Typography>
+                        <div className="border-l-4 border-solid border-blue-500 bg-gray-200 mt-4 py-3 px-2">
+                            <Typography>
+                                <sapn className="font-medium">Hello, Rafael Tivane</sapn><br/>
+                                <span className={classes.quote}>
+                                    A vida e feita de escolhas...
+                                </span>
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 };
