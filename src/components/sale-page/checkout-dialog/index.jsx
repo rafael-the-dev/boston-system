52a52b724d3822197b1be8bbace14fcf4f57804a
Rafael Tivane <luislangabusiness@gmail.com>
2022-10-27 17:38:28 +0200

    memorize go back button

--- src/components/sale-page/checkout-dialog/index.jsx
+++ src/components/sale-page/checkout-dialog/index.jsx
@@ -3,6 +3,8 @@ import { Button, Hidden, Typography } from "@mui/material"
 import classNames from "classnames";
 
 import AddIcon from '@mui/icons-material/Add';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import DoneIcon from '@mui/icons-material/Done';
 
 import classes from "./styles.module.css";
 import { CheckoutContext } from "src/context";
@@ -21,6 +23,16 @@ const CheckoutContainer = ({ onOpen }) => {
         <Hidden smDown>
             <AddIcon />
         </Hidden>
+    ), []);
+
+    const backButtonMemo = React.useMemo(() => (
+        <Button
+            className="border-red-500 mr-4 text-red-500 hover:bg-red-500 py-2 px-4 hover:border-red-500 hover:text-white"
+            onClick={closeHandler}
+            startIcon={<ArrowBackIcon />}
+            variant="outlined">
+            Voltar
+        </Button>
     ), [])
 
     const heaaderMemo = React.useMemo(() => (
@@ -42,40 +54,51 @@ const CheckoutContainer = ({ onOpen }) => {
             onClose={onClose}
             onOpen={onOpen}>
             { heaaderMemo }
-            <div className="py-8 px-5">
-                {
-                    methodsLength === 0 ? (
-                        <Typography
-                            className="font-bold mb-4 text-center text-lg md:text-2xl">
-                            Sem metodos de pagamento 
-                        </Typography>
-                    ) : (
-                        <ul className="">
-                            {
-                                getPaymentMethods().methods?.map(item => (
-                                    <PaymentMethod { ...item } key={item.id} />
-                                ))
-                            }
-                        </ul>
-                    )
-                }
-                <div>
-                    <Typography 
-                        component="h3"
-                        className={classNames("font-semibold uppercase",
-                        methodsLength === 0 ? "text-center" : "text-right")}>
-                        { restAmout > 0 ? (methodsLength === 0 ? "Total" : "falta") : "" }
-                        <span className="ml-2 text-red-500 md:text-2xl">{ restAmout }MT</span>
-                    </Typography>
+            <div className={classNames(classes.content, "flex flex-col items-stretch justify-between py-8 px-5")}>
+                <div className="">
+                        {
+                            methodsLength === 0 ? (
+                                <Typography
+                                    className="font-bold mb-4 text-center text-lg md:text-2xl">
+                                    Sem metodos de pagamento 
+                                </Typography>
+                            ) : (
+                                <ul className="">
+                                    {
+                                        getPaymentMethods().methods?.map(item => (
+                                            <PaymentMethod { ...item } key={item.id} />
+                                        ))
+                                    }
+                                </ul>
+                            )
+                        }
+                        <div>
+                            <Typography 
+                                component="h3"
+                                className={classNames("font-semibold uppercase",
+                                methodsLength === 0 ? "text-center" : "text-right")}>
+                                { restAmout > 0 ? (methodsLength === 0 ? "Total" : "falta") : "" }
+                                <span className="ml-2 text-red-500 md:text-2xl">{ restAmout }MT</span>
+                            </Typography>
+                        </div>
+                        { restAmout > 0 && (<div className="flex justify-center mt-4">
+                            <Button
+                                className="bg-blue-500 py-3 px-4 text-white hover:bg-blue-700"
+                                onClick={clickHandler}
+                                startIcon={addIconMemo}>
+                                Adicionar novo metodo de pagamento
+                            </Button>
+                        </div> )}
                 </div>
-                { restAmout > 0 && (<div className="flex justify-center mt-4">
+                { restAmout === 0 && <div className="flex justify-end">
+                    { backButtonMemo }
                     <Button
-                        className="bg-blue-500 py-3 px-4 text-white hover:bg-blue-700"
-                        onClick={clickHandler}
-                        startIcon={addIconMemo}>
-                        Adicionar novo metodo de pagamento
+                        className="bg-blue-700 py-2 px-4 text-white hover:bg-blue-500"
+                        startIcon={<DoneIcon />}
+                        variant="contained">
+                        Concluir
                     </Button>
-                </div> )}
+                </div> }
             </div>
         </Dialog>
     );
