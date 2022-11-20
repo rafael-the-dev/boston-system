c1993ef1b24baa102825f942ae369b25e0d87a63
Rafael Tivane <luislangabusiness@gmail.com>
2022-11-20 20:42:49 +0200

    enhance user's card

--- src/components/users-page/user-card/index.jsx
+++ src/components/users-page/user-card/index.jsx
@@ -1,15 +1,18 @@
 import classNames from "classnames";
-import { Typography } from "@mui/material";
+import { Button, Chip, Typography } from "@mui/material";
+
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 import classes from "./styles.module.css";
 
 import Image from "src/components/default-image";
+import Link from "src/components/link"
 
-const Card = ({ firstName, image, lastName, username }) => {
+const Card = ({ category, firstName, image, lastName, username }) => {
     const fullName = `${firstName} ${lastName}`;
 
     return (
-        <div className={classNames("border border-solid border-stone-200 mb-6")}>
+        <div className={classNames("border border-solid border-stone-200 items-stretch mb-6 sm:flex")}>
             <div className={classNames(classes.imageContainer, "relative")}>
                 <Image 
                     alt={fullName}
@@ -17,11 +20,26 @@ const Card = ({ firstName, image, lastName, username }) => {
                     src={`/images/users/${image}`}
                 />
             </div>
-            <div className="px-3 py-4">
-                <Typography
-                    component="h2">
-                    { fullName }
-                </Typography>
+            <div className="flex flex-col justify-between px-3 py-4 sm:grow">
+                <div>
+                    <Typography
+                        component="h2">
+                        { fullName }
+                    </Typography>
+                </div>
+                <div className="flex items-center justify-between w-full">
+                    <Chip 
+                        label={category} 
+                    />
+                    <Link href={`profile/${username}`}>
+                        <Button
+                            className="bg-blue-500 capitalize py-2 px-3 text-white hover:bg-blue-700"
+                            endIcon={<ArrowRightAltIcon />}
+                            variant="contained">
+                            View profile
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
