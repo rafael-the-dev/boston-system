6dc15fd82e3c0f00c0a223111f80ebad7422363b
Rafael Tivane <luislangabusiness@gmail.com>
2022-11-27 22:38:54 +0200

    update profit

--- src/pages/api/products/[id]/index.js
+++ src/pages/api/products/[id]/index.js
@@ -1,5 +1,10 @@
+const currency = require("currency.js");
+
+const Error404 = require("src/models/server/errors/404Error")
+
 const { apiHandler } = require("src/helpers/api-handler")
 const { query } = require("src/helpers/db")
+const { getTotalPrice } = require("src/helpers/price")
 
 const requestHandler = async (req, res) => {
 
@@ -18,13 +23,18 @@ const requestHandler = async (req, res) => {
         case "PUT": {
             const { body } = req; 
             const { available, barCode, category, date, sellVat, name, purchasePrice, purchaseVat, sellPrice  } = JSON.parse(body);
-            const values = [ available, barCode, category, date, sellVat, name, purchasePrice, purchaseVat, sellPrice, id ];
 
             return query(`SELECT * FROM Produto WHERE idProduto=?`, [ id ])
                 .then(result => {
-                    if(result.length === 0) throw new Error("Product not found.");
+                    if(result.length === 0) throw new Error404("Product already exists");
 
-                    return query(`UPDATE Produto SET Estado=?, BarCod=?, fk_grupo=?, Data=?, Iva_venda=?, Nome=?, Preco_compra=?, IVA_compra=?, Preco_venda=? WHERE idProduto=?`, values)
+                    const totalPurchasePrice = getTotalPrice({ price: purchasePrice, taxRate: purchaseVat });
+                    const totalSellPrice = getTotalPrice({ price: sellPrice, taxRate: sellVat});
+                    const profit = currency(totalSellPrice).subtract(totalPurchasePrice).value;
+
+                    const values = [ available, barCode, category, date, sellVat, name, purchasePrice, purchaseVat, sellPrice, profit, id ];
+
+                    return query(`UPDATE Produto SET Estado=?, BarCod=?, fk_grupo=?, Data=?, Iva_venda=?, Nome=?, Preco_compra=?, IVA_compra=?, Preco_venda=?, Profit=? WHERE idProduto=?`, values)
                         .then(() => res.json({ message: "Product was successfully updated" }));
                 })
         }
