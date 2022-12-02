f5381209e2e11c756f6aa0ec43dae6ae8d3fce08
Rafael Tivane <luislangabusiness@gmail.com>
2022-12-02 05:19:43 +0200

    change db name

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ module.exports = {
         DB: {
             connectionLimit: 10,
             host     : 'localhost',
-            user     : 'root',
+            user     : 'nanc_root',
             password : "40260101",
-            database : 'nancy_mydb'
+            database : 'nanc_mydb'
         },
         JWT_SECRET_KEY: "53a0d1a4174d2e1b8de701437fe06c08891035ed4fd945aef843a75bed2ade0657b3c4ff7ecd8474cb5180b2666c0688bbe640c9eb3d39bb9f2b724a10f343c6",
         LOCAL_STORAGE: "__cybersys-stock-management-app__",
